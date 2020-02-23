008146f3aedfe85c98e0d92a7816f6a0144b5658
yoonsbi <yhyhk20@naver.com>
2020-02-23 18:49:01 +0900

    Updates

---
+++ precache-manifest.bd541dd67023e076f2e241fa1c674076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef936a68830fc4fc2efd505c7ecfbbb",
+    "url": "/js-test/index.html"
+  },
+  {
+    "revision": "bb4ac79db1b81c4260c0",
+    "url": "/js-test/static/css/main.e8975336.chunk.css"
+  },
+  {
+    "revision": "6b55648ed18c3a11cccb",
+    "url": "/js-test/static/js/2.b28a48d4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/js-test/static/js/2.b28a48d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4ac79db1b81c4260c0",
+    "url": "/js-test/static/js/main.a8361ce5.chunk.js"
+  },
+  {
+    "revision": "da67d54dec4b7a17c6a9",
+    "url": "/js-test/static/js/runtime-main.821dd741.js"
+  }
+]);
